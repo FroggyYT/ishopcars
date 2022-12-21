215fcf1a39ce7a16b70c793201ea6c4fd1caf5b9
FroggyYT <lukebb0215@gmail.com>
2022-12-21 22:43:36 +0000

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navbar
